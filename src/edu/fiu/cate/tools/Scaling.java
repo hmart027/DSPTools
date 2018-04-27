9649c6b9fba51f6a9db944c88d648e6fdff4279c
Harold Martin <harold.martin92@gmail.com>
2018-04-27 08:45:27 -0400

    Starting to add Signal Scaling support

---
+++ src/edu/fiu/cate/tools/Scaling.java
@@ -0,0 +1,15 @@
+package edu.fiu.cate.tools;
+
+public class Scaling {
+	
+	public static float[] decimate(float[] signal, float f0, float f1){
+		
+		return null;
+	}
+	
+	public static float[] interpolate(float[] signal, float f0, float f1){
+		
+		return null;
+	}
+
+}
